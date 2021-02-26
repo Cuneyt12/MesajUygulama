dc0d2168937d8149a68598f1d9c57f23471839d6
Cuneyt12 <cuneytokmenn@gmail.com>
2021-02-26 12:19:11 +0300

    delete comment lines

--- src/mesaggeapp/bin/Config.java
+++ src/mesaggeapp/bin/Config.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mesaggeapp.bin;
 
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Region;
 
-/**
- *
- * @author Cüneyt
- */
 public class Config {
     public static void setAnchorPaneSize(Region region, double bottom, double left, double right, double top){
         AnchorPane.setBottomAnchor(region, bottom);
--- src/mesaggeapp/profil/Profil.java
+++ src/mesaggeapp/profil/Profil.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package mesaggeapp.profil;
 
 import java.io.IOException;
@@ -12,10 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import mesaggeapp.bin.Config;
 
-/**
- *
- * @author Cüneyt
- */
 public class Profil extends AnchorPane {
     
     public Profil() {
